c62b7fd16ef74ec07137558258e5d975a6b478ff
Manuel <mlm09kdev@gmail.com>
2018-08-08 22:01:25 -0400

    Initial commit

---
+++ app/src/main/java/mlm09kdev/com/bestquotes/Data/QuoteData.java
@@ -0,0 +1,18 @@
+package mlm09kdev.com.bestquotes.Data;
+
+import java.util.ArrayList;
+
+import mlm09kdev.com.bestquotes.Model.Quote;
+
+/**
+ * Created by Manuel Montes de Oca on 8/8/2018.
+ */
+public class QuoteData {
+    ArrayList<Quote> arrayList = new ArrayList<>();
+
+    public void getQoutes() {
+
+        String url = "";
+
+    }
+}
---
+++ app/src/main/java/mlm09kdev/com/bestquotes/Model/Quote.java
@@ -0,0 +1,26 @@
+package mlm09kdev.com.bestquotes.Model;
+
+/**
+ * Created by Manuel Montes de Oca on 8/8/2018.
+ */
+public class Quote {
+    private String quote;
+    private String author;
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
