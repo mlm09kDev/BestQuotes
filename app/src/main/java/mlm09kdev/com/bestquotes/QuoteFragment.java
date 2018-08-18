13d0f53ee2022cbdf14be146cb3b6d4057032e42
Manuel <mlm09kdev@gmail.com>
2018-08-17 22:03:25 -0400

    Remove unused colors

--- app/src/main/java/mlm09kdev/com/bestquotes/QuoteFragment.java
+++ app/src/main/java/mlm09kdev/com/bestquotes/QuoteFragment.java
@@ -9,11 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.concurrent.ThreadLocalRandom;
-
-import mlm09kdev.com.bestquotes.Model.Quote;
 
 
 /**
